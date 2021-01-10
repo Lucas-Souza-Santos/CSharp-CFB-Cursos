7290237c7f6c785c4cfd4460cd1933e880030a3b
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-10 00:49:56 -0300

    pronto

---
+++ Exercicios/Ex001.cs
@@ -0,0 +1,8 @@
+using System;
+// Faça um Programa que mostre a mensagem "Alo mundo" na tela
+
+class Exercicios{
+    static void Main(){
+        Console.WriteLine("Alo mundo");
+    }
+}
---
+++ Introducao/Aula12/aula12.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Aula12{
+
+    static void Main(){
+        int nota = 80;
+        /*
+            Operadores relacionais
+                > maior
+                < menor
+                >= maior igual
+                <= menor igual
+                == igual 
+                != diferente
+        */
+        if(nota >= 60){
+            Console.WriteLine("Aprovado");
+        }
+    }
+}
