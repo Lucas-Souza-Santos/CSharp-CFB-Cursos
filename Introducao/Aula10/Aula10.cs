b9a7e8d8495eef4446a1088b1fc083f211b662ae
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-10 00:21:22 -0300

    subindo arquivos

--- Introducao/Aula10/Aula10.cs
+++ Introducao/Aula10/Aula10.cs
@@ -1,7 +1,13 @@
 using System;
 
 class Aula10{
+
+    enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado}
     static void Main(){
-        
+
+        DiasSemana ds = DiasSemana.Domingo;
+
+        Console.WriteLine(ds);
+
     }
 }
---
+++ Introducao/Aula11/aula11.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula11{
+    static void Main(){
+        int n1 = 10;
+        float n2 = 5.4f; 
+        int n3 = (int)n2; // convertendo um valor float em int
+
+        Console.WriteLine(n3);
+    }
+}
