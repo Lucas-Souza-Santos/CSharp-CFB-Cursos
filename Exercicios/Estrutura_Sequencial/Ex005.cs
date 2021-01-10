f10d770b5b6c59580535727268435cfa59825afd
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-10 20:05:33 -0300

    Exercício 005

---
+++ Exercicios/Estrutura_Sequencial/Ex005.cs
@@ -0,0 +1,16 @@
+using System;
+// Faça um Programa que converta metros para centímetros.
+
+class Exercicios
+{
+    static void Main()
+    {   
+        // Pedindo pro usuário digitar o metro e pegando o valor e armazenando na variavel metro
+        Console.Write("Informe o metro para ser convertido: ");
+        float metro = float.Parse(Console.ReadLine());
+
+        float centimetro = (metro * 100);
+
+        Console.WriteLine("{0}m = {1}cm", metro, centimetro); 
+    }
+}
