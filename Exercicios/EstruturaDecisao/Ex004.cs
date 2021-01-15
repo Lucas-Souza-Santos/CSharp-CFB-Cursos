0750af184ee9a71d40891cf636719331c8ae1f92
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-14 21:09:04 -0300

    Exercício 004 Incompleto

---
+++ Exercicios/EstruturaDecisao/Ex004.cs
@@ -0,0 +1,13 @@
+using System;
+// Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
+namespace EstruturaDecisao
+{
+    class Ex004
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
