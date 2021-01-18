2d0e3e291d39b01bbb1580337c0072c7c8335ed2
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-17 23:24:38 -0300

    final da aula 20

---
+++ Introducao/Aula20/Aula20.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Aula20
+{
+    class Aula20
+    {
+        static void Main()
+        {
+            Console.Clear();
+            int cont = 1;    
+
+            while (cont <= 10)
+            {
+                Console.Write(cont + " ");
+                cont++;
+            }            
+        }
+    }
+}
