f3e7e3b0ef3b50011fd295704f37c49a53150b17
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-17 23:36:23 -0300

    final da aula 21

---
+++ Introducao/Aula21/Aula21.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Aula21
+{
+    class Aula21
+    {
+        static void Main()
+        {
+            Console.Clear();
+            int i = 0;
+            
+            do
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            while (i < 10);
+        }
+    }
+}
