85cd6d517b930a74c5ff391b8e049486cd74884b
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-14 19:56:24 -0300

    final aula 29

---
+++ Intermediario/Aula29/Aula29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula29
+{
+    public class Jogador
+    {
+        public int energia;
+        public bool vivo;
+
+        public Jogador()
+        {
+            this.energia = 100;
+            this.vivo = true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador Lucas = new Jogador();
+
+            Console.Write(Lucas.energia);
+        }
+    }
+}
