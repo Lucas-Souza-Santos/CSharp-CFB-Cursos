dd86c0f5c9eb25905fb74204f1575900f41b3ed0
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-14 20:11:32 -0300

    subindo aula 29

--- Intermediario/Aula29/Aula29/Program.cs
+++ Intermediario/Aula29/Aula29/Program.cs
@@ -7,16 +7,23 @@ namespace Aula29
         public int energia;
         public bool vivo;
 
+    // método construtor
         public Jogador()
         {
             this.energia = 100;
             this.vivo = true;
         }
+    // método destrutor
+        	~Jogador()
+            {
+                Console.WriteLine("Jogador foi destruido");
+            }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Jogador Lucas = new Jogador();
 
             Console.Write(Lucas.energia);
