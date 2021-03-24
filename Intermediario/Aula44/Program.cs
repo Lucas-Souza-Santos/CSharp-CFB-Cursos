b80f9a12bf7184549de2f995b4d825be2405b8b1
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-24 05:37:54 -0300

    Aula 44 final

---
+++ Intermediario/Aula44/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula44
+{
+    class Program
+    {
+        struct Carro
+        {
+            public string marca;
+            public string modelo;
+            public string cor; 
+            public Carro(string marca, string modelo, string cor)
+            {
+                this.marca = marca;
+                this.modelo = modelo;
+                this.cor = cor;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro("VW", "Golf", "Branco");
+            Console.Clear();
+            Console.WriteLine($"Marca: {c1.marca} \nModelo: {c1.modelo} \nCor: {c1.cor} ");
+        }
+    }
+}
