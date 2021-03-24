81a9b70034d898a5a4c302a4e56e31503f00dfc0
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-24 06:18:16 -0300

    final da aula 45

--- Intermediario/Aula45/Program.cs
+++ Intermediario/Aula45/Program.cs
@@ -4,22 +4,37 @@ namespace Aula45
 {
     struct Carro
         {
-            public string marca;
-            public string modelo;
             public string cor; 
-            public Carro(string marca, string modelo, string cor)
+            public Carro(string cor)
             {
-                this.marca = marca;
-                this.modelo = modelo;
                 this.cor = cor;
             }
+
+            public void info(int n)
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine($"Cor do Carro {n}: {this.cor}");
+            }
         }
-        
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             
+            Carro[] carros = new Carro[5];
+            carros[0].cor = "Azul";
+            carros[1].cor = "Branco";
+            carros[2].cor = "Vermelho";
+            carros[3].cor = "Preto";
+            carros[4].cor = "Prata";  
+
+            for(int i = 0; i < carros.Length; i++)
+            {
+                carros[i].info(i);
+            }  
+            Console.WriteLine("-------------------------");       
         }
     }
 }
