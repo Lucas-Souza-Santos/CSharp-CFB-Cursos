9ccd5e96c0bdc82f381bcf28346cc441f0347597
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-03-24 05:55:05 -0300

    Aula

---
+++ Intermediario/Aula45/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula45
+{
+    struct Carro
+        {
+            public string marca;
+            public string modelo;
+            public string cor; 
+            public Carro(string marca, string modelo, string cor)
+            {
+                this.marca = marca;
+                this.modelo = modelo;
+                this.cor = cor;
+            }
+        }
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
